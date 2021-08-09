7c429078d52c6a19ae321e969a7cc44982eb3860
boring-km <kms0644804@naver.com>
2021-08-10 00:13:14 +0900

    [main] 간단한 비디오 파일에서 일정 시간 간격으로 이미지 빼오기

---
+++ src/main/java/webp/Main.java
@@ -0,0 +1,17 @@
+package webp;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Mp4Test mp4Test = new Mp4Test();
+        VideoAccess videoAccess = new VideoAccess();
+        File source = videoAccess.getSampleVideo();
+
+        long startTime = System.currentTimeMillis();
+        mp4Test.getThumbnail(source);
+        long endTime = System.currentTimeMillis();
+        System.out.println((endTime-startTime) + "(ms)");
+    }
+}
---
+++ src/main/java/webp/Mp4Test.java
@@ -0,0 +1,34 @@
+package webp;
+
+import java.io.File;
+
+public class Mp4Test {
+
+    /**
+     * 썸네일을 추출하는 메소드
+     *
+     * @param source mp4 file.
+     */
+    public void getThumbnail(File source) throws Exception {
+        double plusSize = 0.2;
+        int threadSize = 8;
+
+        VideoThread[] videoThread = new VideoThread[threadSize];
+
+        for(int i = 0; i < videoThread.length; i++) {
+            videoThread[i] = new VideoThread(source, threadSize, i, plusSize);
+            videoThread[i].start();
+        }
+
+        boolean runFlag = true;
+        while(runFlag) {
+            Thread.sleep(1000);
+
+            runFlag = false;
+            for(int i = 0; i < threadSize; i++) {
+                if(videoThread[i].isAlive())
+                    runFlag = true;
+            }
+        }
+    }
+}
