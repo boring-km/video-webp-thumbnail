05959bd4f5a0ac197738e8f2af749c80babd4d50
boring-km <kms0644804@naver.com>
2021-08-16 19:11:12 +0900

    [main] 동영상 파일 위치와 webp 저장 위치를 설정하여 한번에 webp 파일 생성 성공

--- src/main/java/webp/Main.java
+++ src/main/java/webp/Main.java
@@ -4,10 +4,15 @@ import java.io.File;
 
 public class Main {
 
+    // TODO JavaFX로 Client 프로그램 하나 만들어보기
     public static void main(String[] args) throws Exception {
         String videoPath = "./src/main/resources/sample-mp4-file.mp4";
         File source = new File(videoPath);
         Mp4Video.makeThumbnails(source, 0.1, 4, 120);
-        LoadLibWebp.saveImagesToAnimatedWebp(new String[]{}, 40, "");
+        String[] imageFilePaths = new String[120];
+        for (int i = 0; i < 120; i++) {
+            imageFilePaths[i] = "./images/" + i + ".png";
+        }
+        LoadLibWebp.saveImagesToAnimatedWebp(imageFilePaths, 1200, "./result.webp");
     }
 }
